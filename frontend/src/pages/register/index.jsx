9cc340e15e1895a4a58c05a1d79ce85b6f8362ee
Maysa Figueiredo <maysafig@gmail.com>
2023-09-26 04:46:53 -0300

    feat: criando comunicação da api para salvar o usuário

--- frontend/src/pages/register/index.jsx
+++ frontend/src/pages/register/index.jsx
@@ -1,28 +1,42 @@
 import { Main, Body, Logo, Spacing, CreateAcc } from "./styles"
 import { Input, Button } from "../../components"
 import { useNavigate } from "react-router-dom"
-
+import { useState } from "react"
+import axios from "axios"
 
 const Register = () => {
-    const navigate = useNavigate()
-    return (
-        <Main>
-            <Body>
-                <form>
-                    <Logo>Gerenciador Financeiro</Logo>
-                    <Input label="Nome" placeholder="Digite o nome" required={true} />
-                    <Spacing />
-                    <Input label="Email" placeholder="Digite o email" required={true} type="email" />
-                    <Spacing />
-                    <Input label="Senha" placeholder="Digite a senha" type="password" required={true} />
-                    <Spacing />
-                    <Button label="Criar conta" variant="btn-primary" type="submit"/>
-                    <Spacing />
-                    <CreateAcc onClick={() => navigate("/login")}>Fazer login</CreateAcc>
-                </form>
-            </Body>
-        </Main>
-    )
+	const [data, setData] = useState({
+		name: "",
+		email: "",
+		password: ""
+	})
+
+	const navigate = useNavigate()
+
+	const save = async (ev) => {
+		ev.preventDefault()
+		await axios.post("http://localhost:3001/user/create", data)
+		alert("Usuario criado com sucesso")
+	}
+
+		return (
+		<Main>
+			<Body>
+				<form onSubmit={save}>
+					<Logo>Gerenciador Financeiro</Logo>
+					<Input label="Nome" placeholder="Digite o nome" required={true} value={data.name} onChange={ev => setData({ ...data, name: ev.target.value })}/>
+					<Spacing />
+					<Input label="Email" placeholder="Digite o email" required={true} type="email" value={data.email} onChange={ev => setData({ ...data, email: ev.target.value })} />
+					<Spacing />
+					<Input label="Senha" placeholder="Digite a senha" type="password" required={true} value={data.password} onChange={ev => setData({ ...data, password: ev.target.value })} />
+					<Spacing />
+					<Button label="Criar conta" variant="btn-primary" type="submit" />
+					<Spacing />
+					<CreateAcc onClick={() => navigate("/login")}>Fazer login</CreateAcc>
+				</form>
+			</Body>
+		</Main>
+	)
 }
 
 export default Register
